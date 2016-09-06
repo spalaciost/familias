faf4bee8887dcdc1e26b6a1084823d129ea2768d
Estudiante <Estudiante@10.203.2.111>
2016-09-06 15:46:50 -0500

    ejercicio registro familia

---
+++ src/ejercicio/Persona.java
@@ -0,0 +1,27 @@
+package ejercicio;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Persona {
+    private String nombre;
+    private String Apellido;
+    private int edad;
+    private char genero;
+
+    public Persona(String nombre, String Apellido, int edad, char genero) {
+        this.nombre = nombre;
+        this.Apellido = Apellido;
+        this.edad = edad;
+        this.genero = genero;
+    }
+
+   
+}
