9640e0af4a56e34f63ff56a58411db76ba13fce7
Estudiante <Estudiante@10.203.2.111>
2016-09-06 15:47:48 -0500

    ejercicio registro familia

---
+++ src/ejercicio/Registro.java
@@ -0,0 +1,30 @@
+package ejercicio;
+
+
+import ejercicio.Familia;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Registro {
+    private Familia[] familias;
+
+    public Registro() {
+        this.familias = new Familia[5];
+    }
+    public Persona consultar_persona(String n0ombre){
+        return null;
+    }
+    public void agregar_familia(Familia familia) {
+    }
+    public Familia consultar_familia(String apellido){
+    return null;
+    }  
+}
