6b57cf6fe393cd669e2be28bff0484214587d0af
Estudiante <Estudiante@FI454402-47.unal.loc>
2016-10-04 15:21:23 -0500

    Familia

--- src/ejercicio/Persona.java
+++ src/ejercicio/Persona.java
@@ -1,26 +1,53 @@
 package ejercicio;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Estudiante
- */
 public class Persona {
     private String nombre;
     private String Apellido;
     private int edad;
-    private char genero;
+    private String genero;
 
-    public Persona(String nombre, String Apellido, int edad, char genero) {
+    public Persona(String nombre, String Apellido, int edad, String genero) {
         this.nombre = nombre;
         this.Apellido = Apellido;
         this.edad = edad;
         this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", Apellido=" + Apellido + ", edad=" + edad + ", genero=" + genero + '}';
     }
 
    
